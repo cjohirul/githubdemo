64469fe4ab0e38a9addff42c88fd15f49084ed56
cjohirul <cjohirul71@gmail.com>
2017-12-06 20:29:30 -0500

    message

---
+++ src/mypack/AryanAyra.java
@@ -0,0 +1,12 @@
+package mypack;
+
+public class AryanAyra {
+	public static
+int i = 100;
+string x = "Hello AryanAyra";
+
+	System.out.println(x);
+}
+}
+}
+	
---
+++ src/mypack/First.java
@@ -0,0 +1,11 @@
+package mypack;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to USA  Java Programming Language");
+	}
+
+
+}
